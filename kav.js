8f04f3be67bda1d5a8fb6c87515e657630c20395
yuvaldrori <yuval.drori@gmail.com>
2011-11-27 15:03:02 +0200

    general js

--- kav.js
+++ kav.js
@@ -1,65 +1,26 @@
-var KAV = function () {
+var kav = function () {
+  var group = [];
+  var personalNumber = [];
+  return {
+    getGroup: function () {
+      return group;
+      },
+    addPerson: function (person) {
+      group.push(person);
+      },
+    removePerson: function (personalNuber) {
+      var i = 0;
+      for (i = 0; i < group.length; i = i +1) {
+        if (group[i].id === personalNumber) {
+    group.splice (i, 1);
+	}
+      }
+    }
+  }
+}();
 
-        var l = []; //array of p
-        var ids = []; //array of p.id
-        
-        var sanitizel = function (a) {
-                if (!Array.isArray(a)) {
-                    throw {
-                        name: "userException",
-                        message: "not an array"
-                    };
-                }
-                for (i = 0; i < a.length; i++) {
-                    if (!a[i].id) {
-                        throw {
-                            name: "userException",
-                            message: "p has no id"
-                        };
-                    }
-                    if (ids.indexOf(a[i].id) !== -1) {
-                        throw {
-                            name: "userException",
-                            message: "id " + a[i].id + " already exists"
-                        };
-                    }
-                    ids.push(a[i].id);
-                }
-                l = a;
-                return l;
-            };
-
-        return {
-            init: function (a) {
-                return sanitizel(a);
-            },
-            addp: function (p) {
-                if (!p.id) {
-                    throw {
-                        name: "userException",
-                        message: "p has no id"
-                    };
-                }
-                if (ids.indexOf(p.id) !== -1) {
-                    throw {
-                        name: "userException",
-                        message: "id " + p.id + " already exists"
-                    };
-                }
-                l.push(p);
-                ids.push(p.id);
-                return l;
-            }
-        };
-    }();
-
-try {
-    console.log(KAV.init([{
-        id: 1
-    }, {
-        id: 2
-    }]));
-}
-catch (e) {
-    console.log(e.message);
-}
+kav.addPerson({id:1});
+kav.addPerson({id:2});
+kav.addPerson({id:3});
+kav.removePerson(2);
+console.log(kav.getGroup());
