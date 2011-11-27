d4d5483da2cca45c84f33c787195ec4d54017c49
Yuval Drori <yuval.drori@gmail.com>
2011-11-27 20:25:49 +0200

    add with checks

--- kav.js
+++ kav.js
@@ -1,20 +1,39 @@
 var kav = function () {
-  var group = [];
-  var personalNumber = [];
+  var list = [];
+  var ids = [];
   return {
-    getGroup: function () {
-      return group;
+    getList: function () {
+      return list;
       },
     addPerson: function (person) {
-      group.push(person);
-      },
-    removePerson: function (personalNuber) {
-      var i = 0;
-      for (i = 0; i < group.length; i = i +1) {
-        if (group[i].id === personalNumber) {
-    group.splice (i, 1);
-	}
+      if (!person.id) {
+        return {name: "error",
+                message: "person must have id"};
       }
+      if (ids.indexOf(person.id !== -1)) {
+        return {name: "error",
+                message: "id not unique"};
+      }
+      list.push(person);
+      ids.push(person.id);
+      return 0;
+      },
+    removePerson: function (id) {
+      var i = 0,
+          j = 0;
+      for (i = 0; i < list.length; i = i +1) {
+        if (list[i].id === id) {
+          list.splice (i, 1);
+          for (j = 0; j < ids.length; j = j + 1) {
+            if (ids[j] === id) {
+              ids.splice(j, 1);
+            }
+          }
+        }
+      }
+    },
+    __getIds: function () {
+      return ids;
     }
   }
 }();
@@ -23,4 +42,5 @@ kav.addPerson({id:1});
 kav.addPerson({id:2});
 kav.addPerson({id:3});
 kav.removePerson(2);
-console.log(kav.getGroup());
+console.log(kav.getList());
+console.log(kav.__getIds());
